df3c35c2ca89c90df70037a1d171e2a0be9421c9
gumquat <gumquat@gmail.com>
2023-03-31 08:50:13 -0700

    main.c changes

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include "monty.h"
+
 FILE *file = NULL;
 
 /**
-* main - runs monty bytecode interpreter
-* @argc: argument count
-* @argv: argument vector
-* Return: 0 on success, 1 on failure
-*/
+ * * main - runs monty bytecode interpreter
+ * * @argc: argument count
+ * * @argv: argument vector
+ * * Return: 0 on success, 1 on failure
+ * */
 int main(int argc, char **argv)
 {
 	void (*f)(stack_t **, unsigned int) = NULL;
@@ -17,9 +18,7 @@ int main(int argc, char **argv)
 
 	if (argc != 2)
 		fprintf(stderr, "USAGE: monty file\n"), exit(EXIT_FAILURE);
-
 	file = fopen(argv[1], "r");
-
 	if (!file)
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]), exit(EXIT_FAILURE);
 	for (; getline(&buffer, &bufsize, file) != EOF; line_number++)
@@ -55,15 +54,15 @@ int main(int argc, char **argv)
 
 
 /**
- *  * get_func - finds function to use to execute the desired opcode
- *   * @stack: stack to operate on
- *    * @code: string used to find correct function
- *     * @l: line number of current opcode
- *      * Return: pointer to desired function
- *       */
+ * * get_func - finds function to use to execute the desired opcode
+ * * @stack: stack to operate on
+ * * @code: string used to find correct function
+ * * @l: line number of current opcode
+ * * Return: pointer to desired function
+ * */
 void (*get_func(stack_t **stack, int l, char *code))(stack_t **, unsigned int)
 {
-		instruction_t instruction[] = {
+	instruction_t instruction[] = {
 		{"push", push},
 		{"pall", pall},
 		{"pint", pint},
@@ -71,28 +70,25 @@ void (*get_func(stack_t **stack, int l, char *code))(stack_t **, unsigned int)
 		{"swap", swap},
 		{"add", add},
 		{"nop", nop}
-		};
+	};
+	int i = 0;
 
-		int i = 0;
-
-		while (strcmp(code, instruction[i].opcode) != 0)
-			{
-				i++;
-	
-				if (i > 7)
-				{
-					fprintf(stderr, "L%d: unknown instruction %s\n", l, code);
-					free_stack(stack);
-					err();
-				}
-			}
-		
-		return (instruction[i].f);
+	while (strcmp(code, instruction[i].opcode) != 0)
+	{
+		i++;
+		if (i > 7)
+		{
+			fprintf(stderr, "L%d: unknown instruction %s\n", l, code);
+			free_stack(stack);
+			err();
+		}
+	}
+	return (instruction[i].f);
 }
 
 /**
-* err - error handler
-*/
+ * * err - error handler
+ * */
 void err(void)
 {
 	fclose(file);
@@ -100,33 +96,31 @@ void err(void)
 }
 
 /**
-* pushOp - helper for push operation; sets value for new node
-* @stack: linked list stack to push to
-* @line_number: current line number of bytecode file
-* @pushNum: number to add to new node
-*/
+ * * pushOp - helper for push operation; sets value for new node
+ * * @stack: linked list stack to push to
+ * * @line_number: current line number of bytecode file
+ * * @pushNum: number to add to new node
+ * */
 void pushOp(stack_t **stack, unsigned int line_number, char *pushNum)
 {
 	if (strcmp(pushNum, "0") == 0)
 		(*stack)->n = 0;
-	
 	if (strcmp(pushNum, "0") != 0)
+	{
+		(*stack)->n = atoi(pushNum);
+		if ((*stack)->n == 0 || (pushNum[0] != '-' && (*stack)->n == -1))
 		{
-			(*stack)->n = atoi(pushNum);
-		
-			if ((*stack)->n == 0 || (pushNum[0] != '-' && (*stack)->n == -1))
-				{
-					fprintf(stderr, "Error: L%d: usage: push integer\n", line_number);
-					free_stack(stack);
-					err();
-				}
-		}		
+			fprintf(stderr, "Error: L%d: usage: push integer\n", line_number);
+			free_stack(stack);
+			err();
+		}
+	}
 }
 
 /**
-* free_stack - frees a linked list stack.
-* @stack: stack to free
-*/
+ * * free_stack - frees a linked list stack.
+ * * @stack: stack to free
+ * */
 void free_stack(stack_t **stack)
 {
 	if (!(stack) || !(*stack))
